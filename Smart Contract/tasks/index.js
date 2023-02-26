649405d8acde3221d8cd0f98571d422778895860
Kareem643 <nyumba29@gmail.com>
2023-02-26 21:03:32 +0300

    changes in market.jsx, mint.js. uncomment and check errors that occur. connecting to wallet button working fine.

--- Smart Contract/tasks/index.js
+++
@@ -1,6 +0,0 @@
-
-exports.getAddress = require("./get-address")
-exports.createNft0 = require("./FuchaNft/createNft0")
-exports.createNft1 = require("./FuchaNft/createNft1")
-exports.createNft2 = require("./FuchaNft/createNft2")
-exports.createNft3 = require("./FuchaNft/createNft3")
